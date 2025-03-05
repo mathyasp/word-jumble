2e944d87cd64b3a2d0b4fd74576f1b1dfc4ffcab
mathyasp <mathyastp@gmail.com>
2025-03-05 11:04:15 -0500

    Add boilerplate code to wordjumble.py

--- wordjumble.py
+++ wordjumble.py
@@ -1,1 +1,10 @@
- 
+def load_dictionary(filepath):
+  """
+  Creates hash table from dictionary file
+  Returns: dict where key = sorted letters, value = list of possible words
+  """
+  pass
+
+if __name__ == "__main__":
+  jumbled_words = ['tefon', 'sokik', 'niumem', 'siconu']
+  mac_dictionary = '/usr/share/dict/words'
