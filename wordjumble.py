b95ec37b4b792074f016b4e8b93dccdebed05a46
mathyasp <mathyastp@gmail.com>
2025-03-05 11:18:20 -0500

    Add code to solve jumbled words

--- wordjumble.py
+++ wordjumble.py
@@ -19,7 +19,25 @@ def load_dictionary(filepath):
 
   return words_dict
 
+def solve_jumbles(jumbled_words, dictionary):
+    """
+    Solves individual jumbled words using dictionary lookup
+    Returns: list of solved words
+    """
+    solved_words = []
+    for word in jumbled_words:
+        sorted_word = ''.join(sorted(word))
+        if sorted_word in dictionary:
+            # Take the first solution
+            solved_words.append(dictionary[sorted_word][0])
+    return solved_words
+
 if __name__ == "__main__":
   jumbled_words = ['tefon', 'sokik', 'niumem', 'siconu']
   mac_words = '/usr/share/dict/words'
   words_dict = load_dictionary(mac_words)
+
+  # Solve each jumbled word
+  solved_words = solve_jumbles(jumbled_words, words_dict)
+  print(f"\nJumbled Words: {jumbled_words}")
+  print(f"\nSolved Words: {solved_words}")
